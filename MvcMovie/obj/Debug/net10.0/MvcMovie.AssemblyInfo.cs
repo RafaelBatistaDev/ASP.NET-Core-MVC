a2e32ec5a78b7831746cea6af4ecfead8bd0cdc6
Rafael Batista <rafaelbatistadev@outlook.com.br>
2026-02-08 09:22:49 -0300

    Atualiza projeto e adiciona mudanças

--- MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2665a5bb0da402c238922169c08af976795514")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9069ac9215b557921cb61679964eabc8427e5aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
