7124694f4d514241eb71853dd4deec4327437d79
Rafael Batista <rafaelbatistadev@outlook.com.br>
2026-02-08 12:40:44 -0300

    Atualiza projeto e adiciona mudanças

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; }
+
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+
+    [StringLength(5)]
+    public string Genre { get; set; }
+}
